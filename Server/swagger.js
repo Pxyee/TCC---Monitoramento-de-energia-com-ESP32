9c25d53b3323bf4f50b73d83746957dac23b96cb
Pxyee <claudiasantoss320@gmail.com>
2026-06-16 23:04:31 -0300

    Corrige autenticação Swagger

--- Server/swagger.js
+++ Server/swagger.js
@@ -3,26 +3,30 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const options = {
   definition: {
     openapi: '3.0.0',
+
     info: {
       title: 'VoltSense API',
       version: '1.0.0',
       description: 'API de monitoramento inteligente de energia elétrica'
     },
+
     servers: [
       {
         url: 'https://api.voltsense.com.br'
       }
-    ]
-  },
-  components: { 
-  securitySchemes: {
-    bearerAuth: {
-      type: 'http',
-      scheme: 'bearer',
-      bearerFormat: 'JWT'
+    ],
+
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT'
+        }
+      }
     }
-  }
-},
+  },
+
   apis: ['./index.js']
 };
 
